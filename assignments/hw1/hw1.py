5963c24914a9ad44051515f7faa71d138e839111
Cade Atkins <cadeatkins@gmail.com>
2022-01-19 17:16:01 -0500

    Complete

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,13 +1,11 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Cade Atkins
+Cade Atkins.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Solve problems given in the assignment.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
