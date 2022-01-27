3acc0c1d21922175d882aa16a61789d43a84d168
Cade Atkins <cadeatkins@gmail.com>
2022-01-27 14:56:31 -0500

    Harmonic Mean added

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -9,21 +9,33 @@ I certify that this assignment is entirely my own work.
 
 def means():
     values = eval(input("How many values will you enter: "))
+    result = 1
     value_list = []
-    one_over_x_list = []
-    for i in range(values): ### create list of values
+    one_over_x_list = [] # for harmonic mean
+    x_squared_list = [] # for rms mean
+    for i in range(values): # create list of values
         value = eval(input("Value: "))
         value_list.append(value)
-    for i in value_list: ### harmonic mean
+    for i in value_list: # harmonic mean
         one_over_x = (1 / i)
         one_over_x_list.append(one_over_x)
-    sum_list = sum(one_over_x_list)
+    for i in value_list: # rms mean
+        x_squared = i ** 2
+        x_squared_list.append(x_squared)
+    print(x_squared_list)
+    for i in value_list: # geometric mean
+        result = result * i
+
+    sum_list_harmonic = sum(one_over_x_list)
+    sum_list_rms = sum(x_squared_list)
+    print(sum_list_rms)
+    geometric = float(result)
+
+    print("Harmonic Mean: ", values / sum_list_harmonic)
+    print("Root-Mean-Square Mean: ", (values / sum_list_rms) ** (1/2))
+    print("Geometric Mean: ", geometric ** (1.0 / values))
 
 
-
-
-    print("Harmonic Mean: ", values / sum_list)
-
-
+means()
 
 
