c53f5684cdd2fdc71cfea343bb37ff5baa278980
Cade Atkins <cadeatkins@gmail.com>
2022-02-18 12:24:08 -0500

    help

---
+++ notes/ch5.py
@@ -0,0 +1,25 @@
+# def encode(word):
+#     enlist = []
+#     for letter in word:
+#         enlist.append(ord(letter))
+#     return enlist
+#
+# def decode(num):
+#     unlist = []
+#     for numbers in num:
+#         unlist.append(eval(numbers))
+#     print(unlist)
+#     return unlist
+#
+
+
+halls_food = ['steak', 'fries', 'potatos', 'drinks']
+gilroys_food = ['pizza', 'cheese', 'drinks', 'calzone']
+resteraunts = {
+    'resteraunt': 'food/dishes',
+    'halls': halls_food,
+    'gilroys': gilroys_food,
+}
+resteraunts.items()
+
+
