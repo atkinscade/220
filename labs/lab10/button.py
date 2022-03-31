56552a5b6dd454cb705d555c73b98ecc29a5d153
Cade Atkins <cadeatkins@gmail.com>
2022-03-31 15:26:23 -0400

    instances

---
+++ labs/lab10/button.py
@@ -0,0 +1,31 @@
+class Button:
+
+    def __init__(self, shape, text):
+        self.shape = shape
+        self.text = text
+
+    def get_label(self):
+        return self.text
+
+    def set_label(self, label):
+        self.text = label
+
+    def draw(self, win):
+        self.shape.draw(win)
+        self.text.draw(win)
+
+    def undraw(self):
+        self.shape.undraw()
+        self.text.undraw()
+
+    def is_clicked(self, point):
+        if point in self.shape:
+            return True
+        else:
+            return False
+
+    def color_button(self, color):
+        self.shape.setFill(color)
+
+
+
---
+++ labs/lab10/door.py
@@ -0,0 +1,47 @@
+class Door:
+    def __init__(self, shape, text):
+        self.shape = shape
+        self.text = text
+        self.secret = False
+
+    def get_label(self):
+        return self.text
+
+    def set_label(self, label):
+        self.text = label
+
+    def draw(self, win):
+        self.shape.draw(win)
+        self.text.draw(win)
+
+    def undraw(self):
+        self.shape.undraw()
+        self.text.undraw()
+
+    def is_clicked(self, point):
+        if point in self.shape:
+            return True
+        else:
+            return False
+
+    def open(self, color, label):
+        self.shape.setFill(color)
+        self.text.setText(label)
+
+    def close(self, color, label):
+        self.shape.setFill(color)
+        self.text.setText(label)
+
+    def color_door(self, color):
+        self.shape.setFill(color)
+
+    def is_secret(self):
+        if not self.secret:
+            return False
+        else:
+            return True
+
+    def set_secret(self, secret):
+        self.secret = secret
+
+
